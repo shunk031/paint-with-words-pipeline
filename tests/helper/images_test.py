810d19b66b8683274348d050c058744035456d33
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2022-12-21 00:23:46 +0900

    update tests for images

--- tests/helper/images_test.py
+++ tests/helper/images_test.py
@@ -1,77 +1,40 @@
-EXAMPLE_SETTING_1 = {
-    "color_context": {
-        (0, 0, 0): "cat,1.0",
-        (255, 255, 255): "dog,1.0",
-        (13, 255, 0): "tree,1.5",
-        (90, 206, 255): "sky,0.2",
-        (74, 18, 1): "ground,0.2",
-    },
-    "color_map_image_path": "contents/example_input.png",
-    "input_prompt": "realistic photo of a dog, cat, tree, with beautiful sky, on sandy ground",
-}
+import pytest
+from PIL import Image
 
-EXAMPLE_SETTING_2 = {
-    "color_context": {
-        (0, 0, 0): "dog,1.0",
-        (255, 255, 255): "cat,1.0",
-        (13, 255, 0): "tree,1.5",
-        (90, 206, 255): "sky,0.2",
-        (74, 18, 1): "ground,0.2",
-    },
-    "color_map_image_path": "contents/example_input.png",
-    "input_prompt": "realistic photo of a dog, cat, tree, with beautiful sky, on sandy ground",
-}
+from paint_with_words.helper.images import get_resize_size, resize_image
 
 
-EXAMPLE_SETTING_3 = {
-    "color_context": {
-        (7, 9, 182): "aurora,0.5",
-        (136, 178, 92): "full moon,1.5",
-        (51, 193, 217): "mountains,0.4",
-        (61, 163, 35): "a half-frozen lake,0.3",
-        (89, 102, 255): "boat,2.0",
-    },
-    "color_map_image_path": "contents/aurora_2.png",
-    "input_prompt": "A digital painting of a half-frozen lake near mountains under a full moon and aurora. A boat is in the middle of the lake. Highly detailed.",
-}
-
-EXAMPLE_SETTING_4 = {
-    "color_context": {
-        (7, 9, 182): "aurora,0.5",
-        (136, 178, 92): "full moon,1.5",
-        (51, 193, 217): "mountains,0.4",
-        (61, 163, 35): "a half-frozen lake,0.3",
-        (89, 102, 255): "boat,2.0",
-    },
-    "color_map_image_path": "contents/aurora_1.png",
-    "input_prompt": "A digital painting of a half-frozen lake near mountains under a full moon and aurora. A boat is in the middle of the lake. Highly detailed.",
-}
+@pytest.fixture
+def model_name() -> str:
+    return "CompVis/stable-diffusion-v1-4"
 
 
-ARGNAMES = [
-    "color_context",
-    "color_map_image_path",
-    "input_prompt",
-]
-ARGVALUES = [
-    [EXAMPLE[name] for name in ARGNAMES]
-    for EXAMPLE in [
-        EXAMPLE_SETTING_1,
-        EXAMPLE_SETTING_2,
-        EXAMPLE_SETTING_3,
-        EXAMPLE_SETTING_4,
-    ]
-]
+def test_get_resize_size():
+
+    image = Image.new(mode="RGB", size=(555, 555), color=(0, 0, 0))
+    assert get_resize_size(img=image) == (512, 512)
+
+    image = Image.new(mode="RGB", size=(130, 130), color=(0, 0, 0))
+    assert get_resize_size(img=image) == (128, 128)
+
+    image = Image.new(mode="RGB", size=(70, 70), color=(0, 0, 0))
+    assert get_resize_size(img=image) == (64, 64)
 
 
-def test_get_resize_size(color_map_image_path: str):
+def test_resize_image():
 
-    raise NotImplementedError
+    image = Image.new(mode="RGB", size=(512, 512), color="white")
 
+    resized_image = resize_image(img=image, w=256, h=256)
+    assert resized_image.size == (256, 256)
 
-def test_resize_image(color_map_image_path: str):
-    raise NotImplementedError
+    resized_image = resize_image(img=image, w=128, h=128)
+    assert resized_image.size == (128, 128)
 
-
-def test_flatten_image_importance(color_map_image_path: str):
-    raise NotImplementedError
+    with pytest.raises(AssertionError):
+        resized_image = resize_image(
+            img=image,
+            # Assert if the size is a multiple of 32
+            w=129,
+            h=129,
+        )
