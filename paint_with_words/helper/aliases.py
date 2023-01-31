e13a827bf87301d4030cd2cf8e2814dd35174dfc
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-01-31 11:46:07 +0900

    update aliases

--- paint_with_words/helper/aliases.py
+++ paint_with_words/helper/aliases.py
@@ -1,9 +1,18 @@
-from typing import Dict, Tuple, Union
+from dataclasses import dataclass
+from typing import Dict, List, Tuple, Union
 
 import torch
+import torch as th
 
 from paint_with_words.weight_functions import WeightFunction
 
 RGB = Tuple[int, int, int]
 
 PaintWithWordsHiddenStates = Dict[str, Union[torch.Tensor, WeightFunction]]
+
+
+@dataclass
+class SeparatedImageContext(object):
+    word: str
+    token_ids: List[int]
+    color_map_th: th.Tensor
