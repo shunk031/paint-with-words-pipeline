927019f56b436133d3444b2a2bdd7fdcf4afb5bb
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-02-02 19:08:30 +0900

    update

--- paint_with_words/models/attention.py
+++ paint_with_words/models/attention.py
@@ -24,8 +24,6 @@ def paint_with_words_forward(
     else:
         context_tensor = hidden_states
 
-    # batch_size, sequence_length, _ = hidden_states.shape
-
     query = self.to_q(hidden_states)
 
     key = self.to_k(context_tensor)
@@ -37,6 +35,7 @@ def paint_with_words_forward(
     key = self.reshape_heads_to_batch_dim(key)
     value = self.reshape_heads_to_batch_dim(value)
 
+    # shape: (batch_size * self.heads, 64 * 64, 77)
     attention_scores = th.matmul(query, key.transpose(-1, -2))
 
     attention_size_of_img = attention_scores.shape[-2]
@@ -52,6 +51,34 @@ def paint_with_words_forward(
             cross_attention_weight = 0.0
     else:
         cross_attention_weight = 0.0
+
+    if not isinstance(cross_attention_weight, float):
+        # shape: (batch_size, 64 * 64, 77) -> (batch_size * self.heads, 64 * 64, 77)
+        #
+        # example:
+        # >>> x = torch.arange(20).reshape(2, 10)
+        # tensor([[ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9],
+        #        [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]])
+        #
+        # >>> x.repeat_interleave(2, dim=0)
+        # tensor([[ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9],
+        #         [ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9],
+        #         [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+        #         [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]])
+        #
+        cross_attention_weight = cross_attention_weight.repeat_interleave(
+            self.heads, dim=0
+        )
+
+        # Example:
+        # shape (attention_scores): (16, 4096, 77)
+        #   scores1: (8, 4096, 77), scores2: (8, 4096, 77)
+        # shape (cross_attention_weights): (2, 4096, 77)
+        #   weights1: (1, 4096, 77), weights2: (1, 4096, 77)
+        #
+        # We want to calculate the following:
+        #   scores1 + weights1
+        #   scores2 + weights2
 
     attention_scores = (attention_scores + cross_attention_weight) * self.scale
 
